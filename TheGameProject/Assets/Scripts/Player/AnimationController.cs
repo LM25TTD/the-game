20d2cf8cff2d3494b1a66e73b6b76159812d9a08
Leandro Bentes <lm25ttd@gmail.com>
2017-01-04 19:01:22 -0400

    Basic animations for jump
    Signed-off-by: Leandro Bentes <lm25ttd@gmail.com>

---
+++ TheGameProject/Assets/Scripts/Player/AnimationController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationController : MonoBehaviour
+{
+    
+	private Animation anim;
+	private CharacterController mCharController = null;
+	private Controller mPlayerController = null;
+
+	// Use this for initialization
+	void Start ()
+	{
+		anim = GetComponent<Animation> ();
+		mCharController = GetComponentInParent<CharacterController> ();
+		mPlayerController = GetComponentInParent<Controller> ();
+
+		anim ["Comp|Run"].speed = 2.0f;
+		anim ["Comp|StandA"].speed = 0.5f;
+		anim ["Comp|RunJumpUp"].speed = 2.0f;
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (mPlayerController.ShallMove) {
+			if (mCharController.isGrounded) {
+				anim.Stop ("Comp|RunJumpUp");
+				anim.Play ("Comp|Run");
+			} else {
+				anim.Stop ("Comp|Run");
+				anim.Play ("Comp|RunJumpUp");
+			}
+		} else {
+			anim.Play ("Comp|StandA");	
+		}
+	}
+}
