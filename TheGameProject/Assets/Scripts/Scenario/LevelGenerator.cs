e3b20b2fecb231cd62f815d675173978adbeea5c
Leandro Bentes <lm25ttd@gmail.com>
2017-01-07 15:14:53 -0400

    Implemented infinite scenario
    Signed-off-by: Leandro Bentes <lm25ttd@gmail.com>

---
+++ TheGameProject/Assets/Scripts/Scenario/LevelGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+
+	public GameObject ScenarioDefault = null;
+	public GameObject PreviousObject = null;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OnTriggerEnter(Collider collider)
+	{
+		Vector3 newPosition = transform.position;
+		newPosition.z += 200;
+		if (collider.CompareTag ("Player")) {
+			GameObject newPath = Instantiate (ScenarioDefault, newPosition, transform.rotation);
+			LevelGenerator newPathLevelGenerator = newPath.GetComponent<LevelGenerator> ();
+			if (newPathLevelGenerator != null) {
+				newPathLevelGenerator.PreviousObject = gameObject;
+			}
+			if (PreviousObject != null) {
+				Destroy (PreviousObject);
+			}
+		}
+	}
+
+}
